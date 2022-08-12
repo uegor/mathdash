3f3cc95a73955ed8f0b233520ec41f8fea3bdbcf
gregorop <uegorigor@gmail.com>
2022-08-12 18:35:41 +0300

    база игры

---
+++ game.js
@@ -0,0 +1,10 @@
+let W,H, hero
+
+//сразу после загрузки страницы срабатывает этот код
+addEventListener("DOMContentLoaded", function() {
+
+    W = document.activeElement.clientWidth
+    H = document.activeElement.clientHeight
+    hero = document.getElementsByClassName("hero")[0]
+
+})
